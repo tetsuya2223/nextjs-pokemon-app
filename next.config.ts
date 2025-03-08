eba8e20b7eb6522e4935150b01b09ad77f3c923f
yeah2223 <yeah13261632@gmail.com>
2025-03-08 17:04:34 +0900

    feat:画像のアクセスを許可するための記述を追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ["raw.githubusercontent.com"], // 画像のホストを許可
+  },
 };
 
 export default nextConfig;
